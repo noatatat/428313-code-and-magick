b501ac58b853333694cace030dc84e476f3ecf0f
Vladimir Vorobjev <C:\Users\vorobjev\AppData\Roaming\The Bat!>
2017-11-30 11:27:32 +0300

    feat: открытие и закрытие попапа (без учета фокуса на полее воода)

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var wizardsNumber = 4;
 var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var secondNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -7,8 +10,60 @@ var coatColors = [
   'rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)',
   'rgb(215, 210, 55)', 'rgb(0, 0, 0)'
 ];
-
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizards = [];
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var setupSave = setup.querySelector('.setup-submit');
+var setupUserName = setup.querySelector('.setup-user-name');
+var inputFactor = 0;
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+function onPopupEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+}
+
+function openPopup() {
+  show(setup);
+  document.addEventListener('keydown', onPopupEscPress);
+}
+
+function openPopupOnEnter(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+}
+
+function closePopup() {
+  hide(setup);
+  document.removeEventListener('keydown', onPopupEscPress);
+}
+
+function closePopupOnEnter(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+}
+
+setupOpen.addEventListener('click', openPopup);
+setupOpen.addEventListener('keydown', openPopupOnEnter);
+
+setupClose.addEventListener('click', closePopup);
+setupClose.addEventListener('keydown', closePopupOnEnter);
+
+setupSave.addEventListener('click', closePopup);
+setupSave.addEventListener('keydown', closePopupOnEnter);
 
 function getRandom(min, max) {
   if (!max) {
@@ -34,11 +89,6 @@ function getRandomElementExept(elements, exept) {
     return getRandomElementExept(elements, exept);
   }
 }
-
-var wizards = [];
-
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
 
 var similarWizardTemlate = document.querySelector('#similar-wizard-template');
 var similarWizardItem = similarWizardTemlate.content.querySelector('.setup-similar-item');
@@ -70,10 +120,6 @@ var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizardsNumber; i++) {
   wizards[i] = generateRandomWizard();
   fragment.appendChild(renderWizard(wizards[i]));
-}
-
-function show(element) {
-  return element.classList.remove('hidden');
 }
 
 similarList.appendChild(fragment);
