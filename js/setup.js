95a3e59ce9d25f29be0c0d0ca3b1b59b8ddffe66
Vladimir Vorobjev <C:\Users\vorobjev\AppData\Roaming\The Bat!>
2017-12-05 11:07:03 +0300

    refactor: переносит модуль валидации в файл validate.js и подключает к index.html

--- js/setup.js
+++ js/setup.js
@@ -59,30 +59,6 @@
 })();
 
 (function () {
-  var userNameInput = window.userNameInput;
-  userNameInput.addEventListener('validity', function () {
-    if (userNameInput.validity.tooShort) {
-      userNameInput.setCustomValidity('В имени должно быть не менее 2 символов');
-    } else if (userNameInput.validity.tooLong) {
-      userNameInput.setCustomValidity('В имени должно быть не более 25 символов');
-    } else if (userNameInput.validity.valueMissing) {
-      userNameInput.setCustomValidity('Обязательное поле');
-    } else {
-      userNameInput.setCustomValidity('');
-    }
-  });
-
-  userNameInput.addEventListener('input', function (evt) {
-    var target = evt.target;
-    if (target.value.length < 2) {
-      target.setCustomValidity('В имени должно быть не менее 2 символов');
-    } else {
-      target.setCustomValidity('');
-    }
-  });
-})();
-
-(function () {
   var wizardCoat = window.setup.querySelector('.setup-wizard .wizard-coat');
   var wizardEyes = window.setup.querySelector('.setup-wizard .wizard-eyes');
   var wizardFireBall = window.setup.querySelector('.setup-fireball-wrap');
