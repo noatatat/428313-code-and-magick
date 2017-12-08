aef891da1b10b63ba5f11e2c3a97c40015c251ea
Vladimir Vorobjev <C:\Users\vorobjev\AppData\Roaming\The Bat!>
2017-12-08 12:42:28 +0300

    feat: осуществляет заполнение рюкзака объектами

--- js/setup.js
+++ js/setup.js
@@ -23,4 +23,63 @@
   wizardFireBall.addEventListener('click', function () {
     changeBackgroundColor(wizardFireBall, window.FIRE_BALLS_COLORS);
   });
+
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var shopItems = shopElement.querySelectorAll('img');
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var artifactsElementStartModeHTML = artifactsElement.innerHTML;
+  [].forEach.call(shopItems, function (element) {
+    element.draggable = true;
+  });
+  var shopElementStartModeHTML = shopElement.innerHTML;
+
+  function setOutline(element, flag) {
+    element.style.outline = (flag) ? '2px dashed red' : '';
+  }
+  var draggedItem = null;
+  var dragZoneItem = null;
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+    var artifactsElementItems = artifactsElement.querySelectorAll('.setup-artifacts-cell');
+    function setDragZoneItem() {
+      dragZoneItem = window.utils.getRandomElement(artifactsElementItems);
+      return !dragZoneItem.hasChildNodes()
+        ? dragZoneItem
+        : setDragZoneItem();
+    }
+    setDragZoneItem();
+    setOutline(dragZoneItem, true);
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  document.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+
+    if (evt.target === dragZoneItem) {
+      setOutline(dragZoneItem, false);
+    } else {
+      shopElement.innerHTML = shopElementStartModeHTML;
+      artifactsElement.innerHTML = artifactsElementStartModeHTML;
+      dragZoneItem = null;
+    }
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+  });
 })();
